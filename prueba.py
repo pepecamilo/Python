807f217285895e044c949ef537ffdec15f6e6c34
PepeCamilo <39320387+pepecamilo@users.noreply.github.com>
2021-04-21 20:41:28 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -8,3 +8,22 @@ print("Monty Python.")
 print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
 print("Mi", "nombre", "es", sep="_", end="*")
 print("Monty", "Python.", sep="*", end="*\n")
+print("Me gusta \"Monty Python\"")
+print('i\'m Monty Python.')
+print("i'm Monty Python.")
+print('"Estoy"\n ""aprendiendo""\n """Python"""')
+print(2 ** 3)
+print(2 ** 3.)
+print(2. ** 3)
+print(2. ** 3.)
+print(14 % 4"residuo")
+print(14//4"cociente")
+var = 1
+print(var)
+print("Juan =", juan, "\nMaria =", maria, "\nAdan =", adan, "\nTotalManzanas =", totalManzanas)
+kilometros = 12.25
+millas = 7.38
+millas_a_kilometros = millas*1.61
+kilometros_a_millas = kilometros*0.62
+print(millas, " millas son ", round(millas_a_kilometros, 2), " kilómetros ")
+print(kilometros, " kilómetros son ", round(kilometros_a_millas, 2), " millas "
